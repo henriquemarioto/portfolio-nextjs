0bfe90420d14678f65d9b734dc97fdeea45f2dfb
henriquemarioto <henrique.marioto@outlook.com>
2022-07-01 16:31:52 -0300

    feat: project card componnent

---
+++ src/components/ProjectCard/index.tsx
@@ -0,0 +1,65 @@
+import { FaLink } from 'react-icons/fa';
+import { SiGithub } from 'react-icons/si';
+import {
+  Container,
+  Description,
+  ImageContainer,
+  InfoContainer,
+  Links,
+  Technologies,
+  Title
+} from './style';
+
+interface Props {
+  img?: any;
+  title: string;
+  inProgress?: boolean;
+  description: string;
+  technologies: string[];
+  previewUrl: string;
+  githubUrl: string;
+}
+
+const ProjectCard = ({
+  description,
+  githubUrl,
+  img,
+  previewUrl,
+  technologies,
+  title,
+  inProgress
+}: Props) => (
+  <Container>
+    <ImageContainer img={img}>{!img && <p>In construction</p>}</ImageContainer>
+
+    <InfoContainer>
+      <Title>
+        <h3>{title}</h3>
+        {inProgress && <span>In progress</span>}
+      </Title>
+
+      <Description>{description}</Description>
+
+      <Technologies>
+        <strong>Technologies</strong>: <span>{technologies.join(', ')}</span>
+      </Technologies>
+
+      <Links>
+        <a href={previewUrl}>
+          <FaLink />
+          Preview
+        </a>
+        <a href={githubUrl}>
+          <SiGithub /> Code
+        </a>
+      </Links>
+    </InfoContainer>
+  </Container>
+);
+
+ProjectCard.defaultProps = {
+  img: false,
+  inProgress: false
+};
+
+export default ProjectCard;
---
+++ src/components/ProjectCard/style.ts
@@ -0,0 +1,93 @@
+import styled from 'styled-components';
+
+interface Props {
+  img: any;
+}
+
+export const Container = styled.div`
+  padding: 10px;
+  background-color: ${p => p.theme.colors.white};
+  color: ${p => p.theme.colors.black};
+  border-radius: 5px;
+  min-height: 450px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+export const ImageContainer = styled.div<Props>`
+  width: 100%;
+  height: 240px;
+  border-radius: 5px;
+
+  background-color: ${p => p.theme.colors.grey_800};
+  background-image: url(${p => p.img});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: top center;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  p {
+    font-size: 34px;
+    font-weight: bold;
+  }
+`;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const Title = styled.div`
+  h3 {
+    font-size: 18px;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+
+  span {
+    font-size: 12px;
+    font-weight: bold;
+    color: ${p => p.theme.colors.grey_500};
+  }
+`;
+
+export const Description = styled.p`
+  font-size: 12px;
+`;
+
+export const Technologies = styled.p`
+  font-size: 12px;
+
+  strong {
+    font-weight: bold;
+  }
+
+  span {
+  }
+`;
+
+export const Links = styled.div`
+  font-size: 12px;
+  color: ${p => p.theme.colors.black};
+
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  a {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    color: ${p => p.theme.colors.black};
+
+    svg {
+      width: 15px;
+      height: 15px;
+    }
+  }
+`;
